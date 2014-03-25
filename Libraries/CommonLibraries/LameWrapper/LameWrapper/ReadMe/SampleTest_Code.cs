fcbe7e5a7a9f0f64d6b3652a2cab362978c9882b
cenmengqi85_cp <SND\cenmengqi85_cp>
2014-03-25 03:11:33 +0000

    Sample test code for LameWrapper.

---
+++ Libraries/CommonLibraries/LameWrapper/LameWrapper/ReadMe/SampleTest_Code.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+using LameWrapper;
+
+namespace TestLameWrapper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileStream wavRStream = null;
+            FileStream mp3WStream = null;
+            LameEncodeStream lameStream = null;
+            String testDir = @"D:\Joseph_Data\ProjectData\MyProgram\Cpp\LameWrapper\TestFiles";
+
+            mp3WStream = new FileStream(Path.Combine(testDir, "Test.mp3"), FileMode.Create, FileAccess.Write);
+            lameStream = new LameEncodeStream(mp3WStream);
+
+            wavRStream = new FileStream(Path.Combine(testDir, "Test.wav"), FileMode.Open, FileAccess.Read);
+
+            Byte[] buffer = new Byte[4096*4];
+
+            lameStream.InitialLame();
+            for (Int32 readByte = wavRStream.Read(buffer, 0, buffer.Length);
+                readByte > 0;
+                readByte = wavRStream.Read(buffer, 0, buffer.Length))
+            {
+                if (readByte % 2 == 0)
+                    lameStream.Write(buffer, 0, readByte);
+                else
+                    lameStream.Write(buffer, 0, readByte-1);
+            }
+
+            wavRStream.Close();
+            lameStream.Close();
+            mp3WStream.Close();
+        }
+    }
+}
