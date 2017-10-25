822942185f25287bb8d0dce9252bccee1cc770ad
Joseph cen <josephcen@eurofins.com>
2017-10-25 17:33:51 +0800

    DI_InAspNetCore
    在 Asp.Net core 中 使用 DI 的简单示例。

---
+++ dotNetCore/WebSite/OdeToFood/src/OdeToFood/Greeter.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace OdeToFood
+{
+    public interface IGreeter
+    {
+        string GetGreetMessage();
+    }
+
+    internal class Greeter : IGreeter
+    {
+        private const string GreetKey = "Greeting";
+
+        private IConfiguration _configuration;
+
+        public Greeter(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public string GetGreetMessage()
+        {
+            return _configuration[GreetKey];
+        }
+    }
+}
--- dotNetCore/WebSite/OdeToFood/src/OdeToFood/Startup.cs
+++ dotNetCore/WebSite/OdeToFood/src/OdeToFood/Startup.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
@@ -8,14 +9,31 @@ namespace OdeToFood
 {
     public class Startup
     {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                                .SetBasePath(env.ContentRootPath)
+                                .AddJsonFile("appsettings.json");
+
+            Configuration = builder.Build();
+        }
+
+        private IConfiguration Configuration { get; set; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton(Configuration);
+            services.AddSingleton<IGreeter, Greeter>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(
+                        IApplicationBuilder app, 
+                        IHostingEnvironment env, 
+                        ILoggerFactory loggerFactory,
+                        IGreeter greeter)
         {
             loggerFactory.AddConsole();
 
@@ -26,7 +44,9 @@ namespace OdeToFood
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!!!");
+                var message = greeter.GetGreetMessage();
+
+                await context.Response.WriteAsync(message);
             });
         }
     }
