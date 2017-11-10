26ad6fbd0865e191e952f096b5615dbacaa123d9
Joseph cen <josephcen@eurofins.com>
2017-11-10 11:43:10 +0800

    LearnOWIN_Katana
    用 Console 学习 OWIN (Open Web Interface for .Net) 和 Katana (MS 的关于
     OWIN 的一个实现)。

---
+++ WebSiteApp/ASP5_OWIN_Katana/KatanaIntro/KatanaIntro/Program.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Owin.Hosting;
+using Owin;
+using System;
+
+namespace KatanaIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var uri = "http://localhost:8081";
+
+            using (WebApp.Start<Startup>(uri))
+            {
+                Console.WriteLine("Start!");
+                Console.ReadLine();
+                Console.WriteLine("Stopping...");
+            }
+        }
+    }
+
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.Run(ctx =>
+            {
+                return ctx.Response.WriteAsync("Hello World!");
+            });
+        }
+    }
+}
