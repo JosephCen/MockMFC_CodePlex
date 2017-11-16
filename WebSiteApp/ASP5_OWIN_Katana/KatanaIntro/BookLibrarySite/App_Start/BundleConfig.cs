a511e12bff3d1439d18ddecd76ba621b41293e90
Joseph cen <josephcen@eurofins.com>
2017-11-16 17:50:27 +0800

    Bootstrap_Dropup_FontAweson
    Button group + Dropup 和 Font Aweson 的例子。

--- WebSiteApp/ASP5_OWIN_Katana/KatanaIntro/BookLibrarySite/App_Start/BundleConfig.cs
+++ WebSiteApp/ASP5_OWIN_Katana/KatanaIntro/BookLibrarySite/App_Start/BundleConfig.cs
@@ -25,6 +25,7 @@ namespace BookLibrarySite
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
+                      "~/Content/font-awesome.css",
                       "~/Content/site.css"));
         }
     }
