a5b5aa587a484ad2746d4d5114da9881591e412a
Joseph cen <cenmengqi85@163.com>
2018-06-01 14:32:10 +0800

    Update CopyRight.partial.cs .

--- ConsoleApp/CLQCodeGen/CLQCodeGen/CodeTemplates/SubTemplates/CopyRight/CopyRight.partial.cs
+++ ConsoleApp/CLQCodeGen/CLQCodeGen/CodeTemplates/SubTemplates/CopyRight/CopyRight.partial.cs
@@ -1,20 +1,25 @@
 ﻿using System;
 
-namespace CLQCodeGen.CodeTemplates.SubTemplates
+namespace CLQCodeGen.CodeTemplates.SubTemplates.CopyRight
 {
     public partial class CopyRight
     {
         private readonly string _className;
+
+        private CopyRightData Data { get; set; }
 
         public CopyRight(string className)
         {
             _className = className;
         }
 
-        private string GetClassName() => 
-            _className;
-
-        private string GetCurrentYear() => 
-            DateTime.Now.Year.ToString();
+        protected override void CalculateTemplateData()
+        {
+            Data = new CopyRightData
+            {
+                ClassName = _className,
+                CurrentYear = DateTime.Now.Year.ToString()
+            };
+        }
     }
 }
